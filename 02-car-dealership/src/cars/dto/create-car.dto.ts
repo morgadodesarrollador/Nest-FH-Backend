dc27c5827e51529cc6072ea0142ab61d58e53b3c
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-09-21 22:51:24 +0200

    02-car-dealership-DTO-PipesGlobales

--- 02-car-dealership/src/cars/dto/create-car.dto.ts
+++ 02-car-dealership/src/cars/dto/create-car.dto.ts
@@ -1,9 +1,10 @@
-import { IsString } from "class-validator";
+import { IsString, MinLength } from "class-validator";
 
 export class CreateCarDto {
 
     @IsString({ message: `El campo brand debe de ser un string corto `}) //validador
     readonly brand: string; 
     @IsString()
+    @MinLength(3)
     readonly model: string;
 }
