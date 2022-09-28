6499883d3cff6694de15eed0829502601419ebbc
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-09-28 23:18:11 +0200

    07-poke-api-NewPokemom-ParseMongoIdPipe

--- 07-app-pokemonApi/src/pokemon/pokemon.service.ts
+++ 07-app-pokemonApi/src/pokemon/pokemon.service.ts
@@ -63,9 +63,11 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne( id );
-    // this.pokemonModel.findByIdAndDelete( id );
-    await pokemon.deleteOne();
+    // const pokemon = await this.findOne( id );
+    // // 
+    // await pokemon.deleteOne();
+    const result = this.pokemonModel.findByIdAndDelete( id );
+    return result;
   }
 
   private handleException( error: any ){
