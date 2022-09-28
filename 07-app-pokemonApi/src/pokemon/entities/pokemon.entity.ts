0d0c30ad49f8951269bd2873808f3c8ef756e23e
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-09-28 08:43:06 +0200

    07-poke-api-conexionBD

--- 07-app-pokemonApi/src/pokemon/entities/pokemon.entity.ts
+++ 07-app-pokemonApi/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,26 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+// Hacemos heredar de Document para que la instancia de mi entidad se comporte como un Documento de una colección
+// Decorador para indicar que es un schema de BD
+@Schema() 
+export class Pokemon  extends Document {
+
+    //id lo genera MongoDB
+
+    //nombre del pokemon
+    @Prop({  //reglas de negocio del campo name
+        unique: true,
+        index: true, // aañadimos un indice para hacer filtros, busquedas
+    })
+    name: string;
+
+    //numero de pokemon
+    @Prop({  
+        unique: true,
+        index: true, 
+    })
+    no: number
+}
+
+//Exportamos un esquema basado en esta clase entidad y que le va a decir, al iniciar la BD, que indica a la BD todo
+export const PokemonSchema = SchemaFactory.createForClass( Pokemon )
--- 07-app-pokemonApi/src/pokemon/pokemon.module.ts
+++ 07-app-pokemonApi/src/pokemon/pokemon.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { PokemonService } from './pokemon.service';
-import { PokemonController } from './pokemon.controller';
+  import { PokemonService } from './pokemon.service';
+  import { PokemonController } from './pokemon.controller';
+  import { MongooseModule } from '@nestjs/mongoose';
+  import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
+  imports: [MongooseModule.forFeature([ //indicamos un Modelo
+    {
+      name: Pokemon.name, //este name extiende del Document. Nombre entidad
+      schema: PokemonSchema
+    }
+  ])
+ ] 
 })
 export class PokemonModule {}
