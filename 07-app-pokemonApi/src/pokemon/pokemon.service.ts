fcd2ed6cd175a441e9b7d67eb364767eb97dd664
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-09-28 10:44:42 +0200

    07-poke-api-NewPokemon

--- 07-app-pokemonApi/src/pokemon/pokemon.service.ts
+++ 07-app-pokemonApi/src/pokemon/pokemon.service.ts
@@ -1,12 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ){  }
+
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
-    return createPokemonDto;
+    //createPokemonDto ya se encuentra validado y podemos insertarlo sin problemas
+    try {
+      const pokemon = await this.pokemonModel.create( createPokemonDto );
+      return pokemon;
+    }catch (error) {
+      if (error.code === 11000){   //lanzamos la excepción con un mensaje personalizado
+        throw new BadRequestException(`El Pokemon existe en la BD ${ JSON.stringify(error.keyValue) }`);
+      }
+      console.log(error);   //lanzamos la excepción si es otro error <> 11000
+      throw new InternalServerErrorException(`No se puede crear el Pokemon. Chequear server logs`);
+    }
   }
 
   findAll() {
