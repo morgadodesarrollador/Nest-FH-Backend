672fc25eb78028f5e4c6d42b6e9865473b09cae5
morgadodesarrollador <morgadodesarrollador@gmail.com>
2022-10-12 16:08:59 +0200

    10-TeslaShop-Repositorio: Insertar un Producto

--- 10-tesloshop/src/products/products.service.ts
+++ 10-tesloshop/src/products/products.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ){}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      //crea la instancia del producto con sus propiedades
+      const producto = this.productRepository.create(createProductDto);
+      // Lo graba e impacta en la BD
+      await this.productRepository.save(producto);
+      return producto;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException('Ayuda!')
+    }
   }
 
   findAll() {
